8145a5db9f99a023d0239ced2c070001dfc013d1
danny galimi <dannygalimi13@aim.com>
2021-03-22 23:21:53 -0400

    adding consoles for authenticated users

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -3,14 +3,25 @@ class ConsolesController < ApplicationController
         authentication_required
     end 
 
+    get '/consoles' do
+        @consoles = current_user.consoles
+        erb :"consoles/index.html"
+    end
+
     get '/consoles/new' do 
         erb :"consoles/new.html"
     end
 
-    post 'consoles/' do 
+    post '/consoles' do 
         @console = Console.new
         @console.name = params[:name]
-        @playlist.user = current_user
+        @console.user = current_user
+        
+        if @console.save 
+            redirect '/consoles'
+        else 
+            erb :'consoles/new.html'
+        end
     end 
 
 
