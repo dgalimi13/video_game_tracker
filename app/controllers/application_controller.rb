44e92c3c0d33448813846ec2a473fa42e30b110c
danny galimi <dannygalimi13@aim.com>
2021-03-22 21:13:35 -0400

    email/password working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,3 +13,4 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+ 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
     end 
 
     post '/users' do
-        raise params.inspect
+        @user = User.new
+        @user.email = params[:email]
+        @user.password = params[:password]
+
+    if @user.save 
+        redirect "/"
+    else
+        erb :'users/new.html'
     end
+  end
+
+  
 end  
